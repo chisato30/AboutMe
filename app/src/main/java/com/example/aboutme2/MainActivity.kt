53921352230590504b4ee30e4b485fb04fb4b084
chisato <moterunit@gmail.com>
2022-03-13 10:52:36 +0900

    クイックリスナーをDONEボタンにアタッチ

--- app/src/main/java/com/example/aboutme2/MainActivity.kt
+++ app/src/main/java/com/example/aboutme2/MainActivity.kt
@@ -14,9 +14,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickname(view: View) {
         val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.name_text)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+        }
+
+    findViewById<Button>(R.id.done_button).setOnClickListener {
+        addNickname(it)
     }
+
 }
+
+
 
 
 
